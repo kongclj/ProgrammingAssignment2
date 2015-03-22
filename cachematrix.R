2803baa1692ae87d9005d6b2f1f3711c7233ad3b
James Kong <kongclj@gmail.com>
2015-03-22 08:08:04 +0800

    Made changes to the initial R code file to fulfil the assignment criteria

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overal function : Improving computation of matrix inversion by caching
+## This way, when calculating the inverse of "previously cached matrix",
+## there is not need to compute, but to just take the value from cache.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix method will cache the input matrix
+## caching is done via using the <<- operator to assign variable within that environment
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    get <- function() x
+    set <- function(y) {
+        x <- y 
+        m <<- NULL
+    }
+    setInverse <- function(inv) m <<- inv
+    getInverse <- function() m
+    
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## caccheSolve method will check whether the inverse matrix is in the cache
+## if so, just return the cache 
+## else, solve() matrix to calculate the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+
+    if(!is.null(m)) {
+        return(m)
+    }
+    
+    data <- x$get()
+    m <- solve(data)
+    x$setInverse(m)
+    m
 }
